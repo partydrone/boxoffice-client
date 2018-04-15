4909c8378473f9dc4a2c95aaf44930f6e879aff5
Andy Porter <partydrone@me.com>
2018-04-15 11:47:36 -0600

    Update app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -12,20 +13,23 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'bo'`, async(() => {
+
+  it(`should have as title 'Box Office'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bo');
+    expect(app.title).toEqual('Box Office');
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('should render title in an a tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bo!');
+    expect(compiled.querySelector('a').textContent).toContain('Box Office');
   }));
 });
