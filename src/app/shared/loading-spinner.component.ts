02946b6b973fc20290c44c8f10b402681721de27
Andy Porter <partydrone@me.com>
2018-04-16 02:23:41 -0600

    Create layered loading spinner
    Shiny!

--- src/app/shared/loading-spinner.component.ts
+++ src/app/shared/loading-spinner.component.ts
@@ -2,7 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'bo-loading-spinner',
-  template: `<i *ngIf="loading" class="fal fa-spinner-third fa-spin"></i>`
+  template: `
+  <span *ngIf="loading" class="fa-layers fa-fw">
+    <i class="fal fa-circle" style="color: #ccc;"></i>
+    <i class="fal fa-spinner-third fa-spin"></i>
+  </span>
+  `
 })
 export class LoadingSpinnerComponent {
   @Input() loading: boolean;
