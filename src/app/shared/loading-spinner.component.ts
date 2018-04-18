221bcf8ed58ade16dcfe2c4913195ccb9d5b485f
Andy Porter <partydrone@me.com>
2018-04-17 23:17:35 -0600

    Update spinner styling

--- src/app/shared/loading-spinner.component.ts
+++ src/app/shared/loading-spinner.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   template: `
   <span *ngIf="loading" class="fa-layers fa-fw">
     <i class="fal fa-circle" style="color: #ccc;"></i>
-    <i class="fal fa-spinner-third fa-spin"></i>
+    <i class="fal fa-spinner-third fa-spin" style="color: #33c;"></i>
   </span>
   `
 })
