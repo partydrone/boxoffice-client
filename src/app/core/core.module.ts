e4c3b83680cf2dad60e558cd8b4b8eed55fbe590
partydrone <partydrone@me.com>
2018-05-20 01:17:38 -0400

    Create core module minus authentication

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,16 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { throwIfAlreadyLoaded } from './module-import-guard';
+import { GraphqlModule } from './graphql.module';
 import { MaterialModule } from './material.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    GraphqlModule,
     MaterialModule
   ],
-  exports: [],
-  declarations: [],
+  exports: [
+    GraphqlModule,
+    MaterialModule,
+    NavComponent
+  ],
+  declarations: [
+    NavComponent
+  ],
   providers: []
 })
 export class CoreModule {
---
+++ src/app/core/graphql.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { ApolloLink, concat } from 'apollo-link';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+@NgModule({
+  exports: [ ApolloModule, HttpLinkModule ]
+})
+export class GraphqlModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    const http = httpLink.create({ uri: 'https://elq6gje67nha3ovunm3fnil3gu.appsync-api.us-east-1.amazonaws.com/graphql' });
+    const auth = new ApolloLink((operation, forward) => {
+      operation.setContext({
+        headers: new HttpHeaders().set('x-api-key', 'da2-fyh4vxnk3nc7zg6hypmukgnbhq')
+      });
+
+      return forward(operation);
+    });
+
+    apollo.create({
+      link : concat(auth, http),
+      cache: new InMemoryCache()
+    });
+  }
+}
